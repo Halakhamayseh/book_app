b83ff34cde8914b8095d0e62ea3abe60bf073939
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-05-04 19:07:27 +0300

    complete f1

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS Book;
 
-  CREATE TABLE Book (
+  CREATE TABLE IF NOT EXISTS Book (
       id SERIAL PRIMARY KEY,
     author VARCHAR(255),
-    title VARCHAR(255)
-   
+    title VARCHAR(255),
+   isbn VARCHAR(255),
+   image_url VARCHAR(255),
+   description TEXT
 );
-INSERT INTO Book (author,title) VALUES ('author test','title test');
--- server.js
+++ server.js
@@ -11,23 +11,23 @@ server.use(cors());
 server.use(express.static('public'));
 server.set('view engine', 'ejs');
 const PORT = process.env.PORT || 4500
-const client = new pg.Client(process.env.DATA_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 server.use(express.urlencoded({ extended: true }));
 ///root
 server.get('/', mainHandler);
 server.get('/searches/new', newHandler);
+// server.get('/books/:id', idBookHandler);
 server.post('/searches', searchesHandler);
 server.get('*', errorHandler);
 ///callback funcation
 function mainHandler(req, res) {
-    console.log('hi');
-    let SQL = `SELECT * FROM Book`;
+    // console.log('hi');
+    let SQL = `SELECT * FROM Book;`;
     client.query(SQL)
         .then(result => {
-            console.log(result)
-            res.render('pages/index',{bookKey:result.rows});
-    })
-    
+            // console.log(result)
+            res.render('pages/index',{bookKey: result.rows});
+})
 }
 function newHandler(req, res) {
     res.render('pages/searches/new');
@@ -60,12 +60,15 @@ function Book(obj) {
     let urlPhoto1 = obj.volumeInfo.imageLinks.smallThumbnail;
     let urlPhoto2 = obj.volumeInfo.imageLinks.thumbnail;
     let urlPhoto3 = `https://i.imgur.com/J5LVHEL.jpg`;
-    this.urlPhoto = urlPhoto1 || urlPhoto2 || urlPhoto3;
+    this.image_url = urlPhoto1 || urlPhoto2 || urlPhoto3;
     this.title = obj.volumeInfo.title;
     this.author = obj.volumeInfo.authors;
     this.description = obj.volumeInfo.description;
 }
 //port listen
-server.listen(PORT, () => {
-    console.log(`listen to PORT ${PORT}`)
+client.connect()
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`listen to PORT ${PORT}`)
+        })
 })
